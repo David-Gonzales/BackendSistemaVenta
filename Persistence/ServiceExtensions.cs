61bb49abdfd9d2ec51dac56e54a93c036c75adf4
David Gonzales <david25bmx.dg@gmail.com>
2024-11-15 02:26:59 -0500

    Se agregan cambios del ServiceExtensions de la Persistencia: se quitó un using que no se usaba

--- Persistence/ServiceExtensions.cs
+++ Persistence/ServiceExtensions.cs
@@ -4,7 +4,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Persistence.Contexts;
 using Persistence.Repository;
-using Shared.Services;
 
 namespace Persistence
 {
